55c4aad0f6942045917bb1449b1a02211477043d
faelf <faelferreirasa@gmail.com>
2021-12-27 13:06:57 -0300

    first-commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição do meu site">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name=" author" content="Rafael">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
+    <title>Danki Code - Theme</title>
+</head>
+
+<body>
+    <section class="topo">
+
+        <div class="center">
+            <header>
+                <div class="logo">
+                    <img src="./images/logo.png" alt="logo da product runt">
+                </div>
+            </header>
+            <ul class="menu-desktop">
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+            <div class="clear"></div>
+        </div><!-- /.center -->
+
+    </section><!-- /.topo -->
+</body>
+
+</html>
